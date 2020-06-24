72ec433dacd500eb1ac7ec3c07d742677992b51e
lh <im.lh@hotmail.com>
2020-06-24 17:59:21 +0800

    Initial commit

---
+++ u-admin/src/test/java/com/study/u/UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
